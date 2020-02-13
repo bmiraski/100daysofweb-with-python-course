33ab4c9d2144bf51b40e15766cdb2c4df4ed4f18
Ben Miraski <bmiraski@users.noreply.github.com>
2020-02-13 15:43:20 -0600

    Add files via upload

---
+++ days/001-004-flask-intro/1-flask/program/routes.py
@@ -0,0 +1,18 @@
+from program import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/100days')
+def p100days():
+    return render_template('/100days.html')
+
+
+@app.route('/data')
+def data():
+    return 'Here is some data.'
